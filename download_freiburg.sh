86f233faf1f85968da071c252f83e0f8ce20ac5a
Andrew Wang <andrew.wang27@gmail.com>
2022-02-24 13:30:44 +0000

    download data script

--- download_freiburg.sh
+++ download_freiburg.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-wget -c http://deepscene.cs.uni-freiburg.de/static/datasets/freiburg_forest_annotated.tar.gz.part-aa
-wget -c http://deepscene.cs.uni-freiburg.de/static/datasets/freiburg_forest_annotated.tar.gz.part-ab
-wget -c http://deepscene.cs.uni-freiburg.de/static/datasets/freiburg_forest_annotated.tar.gz.part-ac
-cat freiburg_forest_annotated.tar.gz.part-a* > freiburg_forest_annotated.tar.gz
+#wget -c http://deepscene.cs.uni-freiburg.de/static/datasets/freiburg_forest_annotated.tar.gz.part-aa
+#wget -c http://deepscene.cs.uni-freiburg.de/static/datasets/freiburg_forest_annotated.tar.gz.part-ab
+#wget -c http://deepscene.cs.uni-freiburg.de/static/datasets/freiburg_forest_annotated.tar.gz.part-ac
+#cat freiburg_forest_annotated.tar.gz.part-a* > freiburg_forest_annotated.tar.gz
 
-echo "unzipping..."
-tar -xvzf freiburg_forest_annotated.tar.gz
-rm -f freiburg_forest_annotated.tar.gz*
+#echo "unzipping..."
+#tar -xvzf freiburg_forest_annotated.tar.gz
+#rm -f freiburg_forest_annotated.tar.gz*
 
 echo "moving"
 mv "freiburg_forest_annotated/train/rgb/*" "forest-mask-rcnn/data/freiburg/train/img"
