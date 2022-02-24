97d04650d60c22888a2f99df0620fcfad6acbaea
Andrew Wang <andrew.wang27@gmail.com>
2022-02-24 14:06:38 +0000

    final

--- download_freiburg.sh
+++ download_freiburg.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-#wget -c http://deepscene.cs.uni-freiburg.de/static/datasets/freiburg_forest_annotated.tar.gz.part-aa
-#wget -c http://deepscene.cs.uni-freiburg.de/static/datasets/freiburg_forest_annotated.tar.gz.part-ab
-#wget -c http://deepscene.cs.uni-freiburg.de/static/datasets/freiburg_forest_annotated.tar.gz.part-ac
-#cat freiburg_forest_annotated.tar.gz.part-a* > freiburg_forest_annotated.tar.gz
+wget -c http://deepscene.cs.uni-freiburg.de/static/datasets/freiburg_forest_annotated.tar.gz.part-aa
+wget -c http://deepscene.cs.uni-freiburg.de/static/datasets/freiburg_forest_annotated.tar.gz.part-ab
+wget -c http://deepscene.cs.uni-freiburg.de/static/datasets/freiburg_forest_annotated.tar.gz.part-ac
+cat freiburg_forest_annotated.tar.gz.part-a* > freiburg_forest_annotated.tar.gz
 
-#echo "unzipping..."
-#tar -xvzf freiburg_forest_annotated.tar.gz
-#rm -f freiburg_forest_annotated.tar.gz*
+echo "unzipping..."
+tar -xvzf freiburg_forest_annotated.tar.gz
+rm -f freiburg_forest_annotated.tar.gz*
 
 echo "moving"
 mv freiburg_forest_annotated/train/rgb/* forest-mask-rcnn/data/freiburg/train/img
 mv freiburg_forest_annotated/train/GT_color/* forest-mask-rcnn/data/freiburg/train/mask
 mv freiburg_forest_annotated/test/rgb/* forest-mask-rcnn/data/freiburg/test/img
 mv freiburg_forest_annotated/test/GT_color/* forest-mask-rcnn/data/freiburg/test/mask
+
+echo "deleting"
+rm -rf freiburg_forest_annotated
