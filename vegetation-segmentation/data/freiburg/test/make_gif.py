3593c1b760573da2f31f81d3da1bc95ec4600333
aditya-jain7 <adityajain7@yahoo.co.in>
2022-03-10 01:57:41 +0000

    adi done

--- vegetation-segmentation/data/freiburg/test/make_gif.py
+++ vegetation-segmentation/data/freiburg/test/make_gif.py
@@ -3,15 +3,35 @@ from os import listdir
 from os.path import isfile, join
 import glob
 counter=0
-filenames_preds= glob.glob(r"/Users/adityajain/Documents/Modules IIB/4M25 Advanced robotics/vegetation-segmentation/vegetation-segmentation/data/freiburg/test/preds_plus_real/*.jpg")
+filenames_preds= glob.glob(r"/Users/adityajain/Documents/Modules IIB/4M25 Advanced robotics/vegetation-segmentation/vegetation-segmentation/data/freiburg/test/stacked_preds/*.jpg")
 #filenames_preds=sorted(filenames_preds)
 #filenames_preds=filenames_preds.sort(key = lambda x: int(x.split('.jpg')[0]))
 
 pred_images=[]
+print(len(filenames_preds))
 
 for i in range(len(filenames_preds)):
     #print(counter)
-    
-    pred_images.append(imageio.imread(f'preds_plus_real/{counter}.jpg'))
+    if counter<=27:
+        pred_images.append(imageio.imread(f'img/0{25*counter+300}.jpg'))
+    else:
+        pred_images.append(imageio.imread(f'img/{25*counter+300}.jpg'))
     counter+=1
-imageio.mimsave('combi_preds.gif', pred_images,fps=2)
+imageio.mimsave('orig_images.gif', pred_images,fps=3)
+
+"""for i in range(len(filenames_preds)):
+    #print(counter)
+    
+    pred_images.append(imageio.imread(f'depths/{counter}.jpg'))
+    
+    counter+=1
+imageio.mimsave('depth_images.gif', pred_images,fps=3)"""
+
+"""counter=0
+for i in range(len(filenames_preds)):
+    #print(counter)
+    
+    pred_images.append(imageio.imread(f'stacked_preds/{counter}.jpg'))
+    
+    counter+=1
+imageio.mimsave('preds.gif', pred_images,fps=3)"""
